198f32759d239bb76921333bb2361f701bb0d506
Zhu Meng <z_meng@fanshaweonline.ca>
2020-10-01 14:55:47 -0400

    adding js

--- js/modules/DataModule.js
+++ js/modules/DataModule.js
@@ -1,5 +1,27 @@
 export default  {
-    name: "Trevor",
-    role: "Prof",
-    nickname: "Tervor"
+
+    Justin:{
+        name: "Justin",
+       role: "Coordinator",
+        nickname: "IDK"
+    },
+
+    John:{
+        name: "John",
+       role: "Super chill prof",
+        nickname: "just John"
+    },
+    
+    Joe:{
+        name: "Joe",
+       role: "Prof",
+        nickname: "Teddy Bear"
+    },
+    
+    Pan:{
+        name: "Pan",
+       role: "Code Ninja",
+        nickname: "Spider Pan"
+    }
+
 }
